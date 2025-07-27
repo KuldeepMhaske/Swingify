19ee5a832dc857d0bd0a6b37e24af326267305b9
KULDEEP MHASKE <113102702+KuldeepMhaske@users.noreply.github.com>
2025-07-27 09:43:21 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Bind to PORT from environment (needed for Render)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -9,7 +13,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,12 +21,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Static files (if using WithStaticAssets from Razor Sdk)
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
